d5306a3206f08076454c45d10496f065ba2f24ca
santiagohewett <santiago.hewett@stu.ocsb.ca>
2023-09-19 22:46:25 +0000

    Finished Unit1-04-Python

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Santiago Hewett
+# Created on: Sep. 19, 2023
+# This program calculates and displays the area and perimeter of a
+# rectangle with length 3.8cm and width 7.5cm.
+
+
+def main():
+    print("For a rectangle that has dimensions")
+    print("length 3.8cm and width 7.5cm:")
+    print("")
+    print("Area = {} cm^2".format(3.8 * 7.5))
+    print("Perimeter = {} cm".format(2 * (3.8 + 7.5)))
+
+
+if __name__ == "__main__":
+    main()
